d55d369ec1ea11569f8d98b0adccbbbeddcbdc9f
benwgrant <bwg9sbe@virginia.edu>
2024-01-14 16:07:10 -0500

    Added header with logo and more information about the weather

--- react-weather-app/src/WeatherData.js
+++ react-weather-app/src/WeatherData.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const WeatherData = ({ data }) => {
+const WeatherData = ({ data, zipcode }) => {
   if (!data) return null;
 
   const temperature = data.current.temperature_2m;
   const windSpeed = data.current.wind_speed_10m;
   const humidity = data.current.relative_humidity_2m;
   const apparent_temperature = data.current.apparent_temperature;
+  const cloud_cover = data.current.cloud_cover;
+  const rain = data.current.rain;
+  const gusts = data.current.wind_gusts_10m;
 
   return (
     <div className="weather-widget">
-      <h2>Weather Information</h2>
+      <h2>Weather for {zipcode}</h2>
       <div className="weather-details">
         <div className="weather-column">
           <h3>Temperature</h3>
@@ -21,7 +24,9 @@ const WeatherData = ({ data }) => {
         </div>
         <div className="weather-column">
           <h3>Wind</h3>
-          <p>{windSpeed} MPH</p>
+          <p>{windSpeed} MPH<br />
+            (Gusts up to {gusts} MPH)
+          </p>
           {/* Where I can add more data */}
         </div>
         <div className="weather-column">
