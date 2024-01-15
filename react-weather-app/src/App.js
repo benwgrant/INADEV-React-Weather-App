4a5d43c15463fa1acaa1c9a26e6dc603bbafb5c3
benwgrant <bwg9sbe@virginia.edu>
2024-01-14 20:35:53 -0500

    Changed favicon and added surface level pressure to the weather data. Also added a new css file for the weather data component.

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Weather.css';
 import React, { useState } from 'react';
 import WeatherData from './WeatherData';
 import Header from './Header';
@@ -32,7 +33,7 @@ function App() {
   };
 
   const fetchWeatherData = async (latitude, longitude) => {
-    const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_gusts_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York`;
+    const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York`;
     // const weatherApiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York';
     try {
       const response = await axios.get(weatherApiUrl);
